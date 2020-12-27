91e46c01a4318ef25daf670349bc39081617962e
djaysllan <76598129+djaysllan@users.noreply.github.com>
2020-12-27 12:46:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -468,7 +468,7 @@ async function starts() {
 						const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
 						buff = await client.downloadMediaMessage(encmedia)
 						for (let _ of anu) {
-							client.sendMessage(_.jid, buff, image, {caption: `[ Ini Broadcast ]\n\n${body.slice(4)}`})
+							client.sendMessage(_.jid, buff, image, {caption: `[ Isto é uma transmissão ]\n\n${body.slice(4)}`})
 						}
 						reply('Suksess broadcast')
 					} else {
