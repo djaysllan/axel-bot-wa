fbd2d3fd4da5b139f4e2eefdf3d6d8c6812bdfd7
djaysllan <76598129+djaysllan@users.noreply.github.com>
2020-12-27 21:07:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function starts() {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `Oi @${num.split('@')[0]}\nBem vindo ao grupo *${mdata.subject}*`
+				teks = `Oi @${num.split('@')[0]}\nBem vindo ao grupo, conheça nosso dublador Wa.me/559180266097 *${mdata.subject}*`
 				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			} else if (anu.action == 'remove') {
