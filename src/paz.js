1ada438e44026e9674fa0419d2690fa051fb885f
djaysllan <76598129+djaysllan@users.noreply.github.com>
2020-12-30 16:13:44 -0300

    Create paz.js

---
+++ src/paz.js
@@ -0,0 +1,14 @@
+const egrila = (prefix) => {
+	return `*essa tabela mostra o quão e-girl é a Isabela atualmente*
+10%
+20%
+30% ✅
+40%
+50%
+60%
+70%
+80%
+90%
+100%
+*Voltarei com mais atualizações*
+`
