c88c641051550fdf59a28d6b6f7b883367673c78
djaysllan <76598129+djaysllan@users.noreply.github.com>
2020-12-30 13:27:29 -0300

    Update egrila.js

--- src/egrila.js
+++ src/egrila.js
@@ -1,1 +1,20 @@
-1
+const help = (prefix) => {
+	return `
+*essa tabela mostra o quão e-girl é a Isabela atualmente*
+
+10%
+20%
+30% ✅
+40%
+50%
+60%
+70%
+80%
+90%
+100%
+
+*Voltarei com mais atualizações*
+`
+}
+
+exports.help = help
