740379105926fd6119508be1ec46c7e7c44bbc81
djaysllan <76598129+djaysllan@users.noreply.github.com>
2020-12-30 22:01:50 -0300

    Update dubla.js

--- src/dubla.js
+++ src/dubla.js
@@ -1,1 +1,15 @@
-1
+const dubla = (prefix) => {
+	return `
+*Eduardo Dublador*
+
+O cara dublou as torre gêmeas véi
+*Esse cara é um deus*
+
+Mas não dubla o Mickey nem fudendo 👺 "ain minha garganta"
+*fresco* 👺
+
+Mas porra, as torre gêmeas véi
+`
+}
+
+exports.dubla = dubla
