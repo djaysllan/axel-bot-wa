cc2fcb2bdbbc98c0340ca5445833f54785944906
djaysllan <76598129+djaysllan@users.noreply.github.com>
2020-12-30 22:06:08 -0300

    Update lzz.js

--- src/lzz.js
+++ src/lzz.js
@@ -1,1 +1,18 @@
-1
+const lzz = (prefix) => {
+	return `*essa tabela mostra o quão gado é o Lzz atualmente*
+10%
+20%
+30% 
+40% 
+50%
+60%
+70%
+80%✅
+90%
+100%
+
+*o cara gadeou a mina errada kkkkkkk sfd bicho*
+`
+}
+
+exports.lzz = lzz
