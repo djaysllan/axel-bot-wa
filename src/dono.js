83af5d475b497c2c7d09486feecba2ba619f4e6c
djaysllan <76598129+djaysllan@users.noreply.github.com>
2020-12-30 22:23:26 -0300

    Create dono.js

---
+++ src/dono.js
@@ -0,0 +1,9 @@
+const dono = (prefix) => {
+	return `
+Meu dono é o *Aysllan*
+Amo muito meu dono e com certeza ele não criou esse comando pela carência dele e a falta de amor e carinho
+claramente não foi isso
+`
+}
+
+exports.help = help
