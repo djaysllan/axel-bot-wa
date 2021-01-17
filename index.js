5d46ad9c8584988f86ffb3ea42c7e2f565d9b7d6
djaysllan <76598129+djaysllan@users.noreply.github.com>
2021-01-16 22:04:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -560,8 +560,8 @@ async function starts() {
 				case 'simi':
 					if (args.length < 1) return reply('Escreve algo?')
 					teks = body.slice(5)
-					anu = await simih(teks) //fetchJson(`https://simsumi.herokuapp.com/api?text=[oi]&lang=[pt]})
-					//if (anu.error) return reply('Simi não sabe')
+					anu = await simih(teks) //fetchJson(`https://mhankbarbars.herokuapp.com/api/samisami?text=${teks}`, {method: 'get'})
+					//if (anu.error) return reply('Simi ga tau kak')
 					reply(anu)
 					break
 				case 'simih':
